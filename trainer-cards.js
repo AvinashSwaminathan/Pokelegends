bc4c68147864ea2cb801dc8094b07b79e5f245b0
AvinashSwaminathan <chitradevi2010@gmail.com>
2014-11-10 16:45:42 +0530

    trainer-card.js

---
+++ trainer-cards.js
@@ -0,0 +1,21 @@
+/**
+ * Trainer Cards
+ *
+ * This is where the trainer cards commands
+ * are located.
+ *
+ */
+
+var trainerCards = {
+
+	beforemath: 'judgmental',
+	judge: 'judgmental',
+	elitefourjudge: 'judgmental',
+	judgmental: function (target, room, user) {
+	    if (!this.canBroadcast()) return;
+	    this.sendReplyBox('<center><img src="http://i.imgur.com/uPqFgYD.png"><br><font color="blue"><blink>Ace: Lapras</blink><br><font color="green">Stay Tuned!');
+	},
+};
+
+Object.merge(CommandParser.commands, trainerCards);
+exports.trainerCards = trainerCards;
